a06f148dade2d8d149805b42b152ccda4674513e
haseebc <haseebchaudhary@gmail.com>
2022-05-03 21:09:53 +0200

    simple push

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,8 +2,11 @@ class RegistrationsController < ApplicationController
 
     before_action :configure_permitted_parameters, if: :devise_controller?
   
+
+  
     protected
   
+
     def configure_permitted_parameters
       # For additional fields in app/views/devise/registrations/new.html.erb
       devise_parameter_sanitizer.permit(:sign_up, keys: %i[firstname lastname company])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,22 @@
 class UsersController < ApplicationController
     def edit
         @user = current_user
-      end
+    end
     
-      def update
-        @user = current_user
-        if @user.update(user_params)
-          redirect_to dashboard_profile_path
-        else
-          flash[:alert] = 'An error occured.'
-          render :edit
-        end
+    def update
+      @user = current_user
+      if @user.update(user_params)
+        redirect_to dashboard_profile_path
+      else
+        flash[:alert] = 'An error occured.'
+        render :edit
       end
-    
-      private
-    
-      def user_params
-        params.require(:user).permit(:firstname, :lastname, :email, :company, :profilepicture)
-      end
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:firstname, :lastname, :email, :company, :profilepicture)
+    end
     
 end
